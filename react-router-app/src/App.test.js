0726ae541939ca14ca2c86b7ea3787ee5230f8ab
minkiin <kimin@ovncorp.com>
2019-08-13 11:35:06 +0900

    뎁스3

--- react-router-app/src/App.test.js
+++ react-router-app/src/App.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 // 뎁스1
 // 뎁스2
+// 뎁스3
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
